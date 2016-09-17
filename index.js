edf53311bff162115a53c17e90f7c638e1f28643
xiaody <youyounaihe@gmail.com>
2016-09-17 23:08:09 +0800

    publish commonjs module

--- index.js
+++
@@ -1,8 +0,0 @@
-// not sure what to export...
-module.exports = Object.assign(
-  {
-    Matcher: require('./lib/Matcher')
-  },
-  require('./lib/FilterClasses'),
-  require('./lib/utils')
-)
---
+++ module.js
@@ -0,0 +1,3 @@
+export {default as Matcher} from './lib/Matcher'
+export * from './lib/FilterClasses'
+export * from './lib/utils'
